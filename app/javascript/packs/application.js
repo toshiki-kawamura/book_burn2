94887669045f88d8e3480cf93bc1dcee14c5c1e6
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-11-23 21:58:57 +0900

    基本的な処理の実装は完成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,6 @@ require("jquery")
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../new_form")
-require("@nathanvda/cocoon")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
