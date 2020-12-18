2fb405b40b0ab533bf0ed6f70d2f4c9a5be593c7
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-12-18 13:25:03 +0900

    コンセプト名編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("jquery")
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+//= require cocoon
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
